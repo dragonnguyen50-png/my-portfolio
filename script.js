7e88c4945713c25a3778816bfe06846df89eab18
dragonnguyen50-png <dragonnguyen50@gmail.com>
2026-03-30 09:30:22 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Portfolio của T.Nguyên đã sẵn sàng!');
+    alert('Chào mừng bạn đến với trang Portfolio của mình trên GitHub Pages!');
+});
